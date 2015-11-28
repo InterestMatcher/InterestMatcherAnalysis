e8b60a1e97f612a43465eb577d306a57cedba267
tommypacker <tommypacker@gmail.com>
2015-11-27 21:08:28 -0600

    Add comments

--- Analysis.py
+++ Analysis.py
@@ -5,5 +5,5 @@ from firebaseSecret import AUTHSECRET
 postsUrl = "https://interestmatcher.firebaseio.com/chatrooms/public.json"
 
 r = requests.get(postsUrl + "?print=pretty" + "?auth=" + AUTHSECRET)
-postData = json.dumps(json.loads(r.text), indent=4, sort_keys=True)
+postData = json.dumps(json.loads(r.text), indent=4, sort_keys=True) #JSON of all the posts
 print postData #Debugging purposes only
