2b01c4e705e2c4e0688f528d451dec292cce9e80
tommypacker <tommypacker@gmail.com>
2015-11-28 18:56:27 -0600

    Put all the JSON data into a list

--- Analysis.py
+++ Analysis.py
@@ -5,5 +5,10 @@ from firebaseSecret import AUTHSECRET
 postsUrl = "https://interestmatcher.firebaseio.com/chatrooms/public.json"
 
 r = requests.get(postsUrl + "?print=pretty" + "?auth=" + AUTHSECRET)
-postData = json.dumps(json.loads(r.text), indent=4, sort_keys=True) #JSON of all the posts
-print postData #Debugging purposes only
+postData = json.loads(r.text) #JSON of all the posts
+posts = [] #Array of all the post content
+
+for postContent in postData.values():
+	posts.append(postContent["content"])
+
+print posts #Debugging purposes only
