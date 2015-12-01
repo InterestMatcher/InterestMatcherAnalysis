acbeccda5ee6c9182dec5f414173745c26cf071e
whrobbins <wrobbins38@gmail.com>
2015-12-01 02:33:38 -0600

    add email digest feature

--- Analysis.py
+++ Analysis.py
@@ -2,6 +2,9 @@ import requests
 import json
 import pprint
 import unicodedata
+import smtplib
+import getpass
+from email.mime.text import MIMEText
 from firebaseSecret import AUTHSECRET
 from alchemyapi import AlchemyAPI
 
@@ -32,7 +35,7 @@ def get_post_content():
 	return posts
 
 def keywords_and_sentiment(contentList):
-	# Accepts a string of posts (or chats), and returns a dictionary with keywords and sentiment
+	# Accepts a string of posts (or chats), and returns a list of keywords
 	alchemyapi = AlchemyAPI()
 	relevanceList = []
 	sentiment = 0
@@ -47,16 +50,50 @@ def keywords_and_sentiment(contentList):
 				if float(keyword['relevance']) > .97:
 					relevanceList.append(keyword['text'])
 				counter += 1
-	print relevanceList
-	return float(sentiment/counter)
+	if DEV_MODE:
+		if not counter is 0:
+			print float(sentiment/counter)
 
+	return relevanceList
+
+def send_digest_email(username, password, recipient):
+	# Accepts strings with the address & password of an email account (IMAP/SMTP enabled) 
+	# and send email to recipient email address
+
+	text = "Hello!  This is your Interest Matcher Email Digest.  " + \
+		"Here are things people have been talking about:\n" + str(keywords_and_sentiment(get_post_content()))
+
+	if DEV_MODE:
+		print text
+
+	# Send the message
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(username, password)
+	server.sendmail(username, recipient, text)
+	server.quit()
 
 # Play around with this in testing - only runs in development mode
 if DEV_MODE:
 	pprint.pprint(get_chat_content()) 
 	print '\n'
+
 	pprint.pprint(get_post_content())
 	print '\n'
+
 	print keywords_and_sentiment(get_post_content())
+	print '\n'
+
+	# NOTE! To login to gmail through IMAP/SMTP, you must enable "less secure apps" in your gmail settings.
+	print "enter sending gmail username: "
+	addr = str(raw_input()) + "@gmail.com"
+	print "enter password"
+	pw = str(getpass.getpass())
+	print "enter recipient email address: "
+	recip = str(raw_input())
+	send_digest_email(addr, pw, recip)
+
+
+
 
 
